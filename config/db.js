ab8dc3c073952aa67993a8fcf6cdd90e102d0e6f
Samarth <samarthbsacharya@gmail.com>
2023-08-18 22:34:01 +0530

    changes made

--- config/db.js
+++ config/db.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 mongoose.set('strictQuery', true);
 
-const mongodb = process.env.mongoURI
-
+const mongodb = process.env.mongoURI;
 
 const connection = mongoose.connect(mongodb)
 module.exports = {
