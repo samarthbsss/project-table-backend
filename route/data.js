a468c48657003291eb402654c30398dc918c71a6
Samarth <samarthbsacharya@gmail.com>
2023-08-19 10:56:54 +0530

    date format changes made

--- route/data.js
+++ route/data.js
@@ -22,37 +22,37 @@ router.get('/data', async (req, res) => {
   });
 
 
-// router.post('/data', async (req, res) => {
-//     try {
-//       const newData = req.body;
-//       const createdData = await DataModel.create(newData);
-//       res.json(createdData);
-//     } catch (error) {
-//       console.error(error);
-//       res.status(500).json({ error: 'Server error' });
-//     }
-//   });
 router.post('/data', async (req, res) => {
-  try {
-    const newData = req.body;
-    const receivedDate = new Date(newData.date);
+    try {
+      const newData = req.body;
+      const createdData = await DataModel.create(newData);
+      res.json(createdData);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Server error' });
+    }
+  });
+// router.post('/data', async (req, res) => {
+//   try {
+//     const newData = req.body;
+//     const receivedDate = new Date(newData.date);
 
-    // Create a new Date object with only the date part (set time to midnight)
-    const dateToSave = new Date(receivedDate.getFullYear(), receivedDate.getMonth(), receivedDate.getDate());
+//     // Create a new Date object with only the date part (set time to midnight)
+//     const dateToSave = new Date(receivedDate.getFullYear(), receivedDate.getMonth(), receivedDate.getDate());
 
-    // Create a new document with the modified date
-    const newDataToSave = {
-      ...newData,
-      date: dateToSave
-    };
+//     // Create a new document with the modified date
+//     const newDataToSave = {
+//       ...newData,
+//       date: dateToSave
+//     };
 
-    const createdData = await DataModel.create(newDataToSave);
-    res.json(createdData);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Server error' });
-  }
-});
+//     const createdData = await DataModel.create(newDataToSave);
+//     res.json(createdData);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: 'Server error' });
+//   }
+// });
   
   router.put('/data/:id', async (req, res) => {
     try {
